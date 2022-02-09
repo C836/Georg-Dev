4abee95eaffaf1492aec7b8183dfcf95b1c2d7cd
Paulo Reis <paauloreis@yahoo.com.br>
2022-02-09 18:00:50 -0300

    pequenas modificacoes css

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('main-button');
+
+button.addEventListener('click', () =>{
+    Event.preventDefault()
+
+    const email = document.getElementById('email')
+    const password = document.getElementById('password')
+    const underlineError = document.getElementById('underline-email-error')
+    const spanError = document.getElementById('error-email') 
+       console.log('clicou');
+})
