769dd28301a487dbd219974cc03cf63e4add6f2e
C836 <gl.sepiol@gmail.com>
2022-02-11 15:44:48 -0300

    página de cadastro

--- cadastro/registro.js
+++ cadastro/registro.js
@@ -1,3 +1,15 @@
+function cadastro(){
+  document.getElementById("insCaixa").style.opacity = 0;
+  document.getElementById("insCaixa").style.visibility = "hidden";
+  document.getElementById("insCad").style.display= "block";
+    setTimeout( function() {
+      document.getElementById("insCaixa").style.display= "none";
+      document.getElementById("insCad").style.display= "block";
+      document.getElementById("insCad").style.opacity = 1;
+      document.getElementById("insCad").style.visibility = "visible";
+        }, 400);
+}
+
 'use strict';
 
 const limparForm = () => {
