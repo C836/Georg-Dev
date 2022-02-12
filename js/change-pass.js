47d2095e78364fad840d55e2dee4cd6d928e2e52
Paulo Henrique Reis <87020059+paulooliveirv@users.noreply.github.com>
2022-02-12 20:58:51 -0300

    melhorando visual

--- js/change-pass.js
+++ js/change-pass.js
@@ -1,14 +1,17 @@
  function validaEmail(){
         const email = document.getElementById('email')
+        const voltar = document.getElementById('voltar')
+        const btn = document.getElementById('enviar_troca_senha')
         const underlineErrorEmail = document.getElementById('underline-email-error')
         const spanError = document.getElementById('error-email') 
+
+        //validando email
         if (email.value == '' || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)){
             
             underlineErrorEmail.classList.add("underlinePadrao");
             spanError.style.display = 'block';
             spanError.style.color = 'red';
             spanError.innerText = 'Digite um email válido'
-            
         }
         else{
             underlineErrorEmail.classList = "underline"
@@ -18,6 +21,9 @@
             spanError.style.color = 'blue';
             spanError.innerText = 'A sua nova senha foi enviada para o seu email'
             email.style.display = 'none';
+            btn.style.display = 'none';
+            voltar.style.display = 'block';
+            
 
         }
     }
